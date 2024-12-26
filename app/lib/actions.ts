78cd21752eba3cf781219b6489e767ddb7624623
Takumi Sakamoto <124575646+mocchio@users.noreply.github.com>
2024-12-26 18:32:59 +0900

    ✨feature: 16章まで実装

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,8 @@
 "use server";
 
+import { signIn } from "@/auth";
 import { sql } from "@vercel/postgres";
+import { AuthError } from "next-auth";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
@@ -27,6 +29,28 @@ export type State = {
   };
   message?: string | null;
 };
+
+/**
+ * 認証
+ */
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
+}
 
 /**
  * 請求書作成
